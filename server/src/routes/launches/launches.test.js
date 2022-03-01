48cd46f3d748f7d0dc1470c8955ac583392d7650
Anvar Genzhaliev <genzhaliev.anvar@gmail.com>
2022-02-28 22:06:41 -0800

    load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,13 @@
 const request = require('supertest');
-
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+
 const app = require('../../app');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
